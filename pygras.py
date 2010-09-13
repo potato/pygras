39ca702c5c3951c11c3aa90e3a2afc8b55828f70
potato <mail@potato.hu>
2010-09-13 08:46:56 +0200

    [enh] vectors are now tuples

--- pygras.py
+++ pygras.py
@@ -4,19 +4,16 @@ import sys
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import math
+from random import random
+from math import sqrt
 
 class Star:
     def __init__(self):
         # position
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
+        self.pos = (0.0, 0.0, 0.0)
 
         # velocity
-        self.vx = 0.0
-        self.vy = 0.0
-        self.vz = 0.0
+        self.v  = (0.0, 0.0, 0.0)
 
         # mass
         self.m = 0.02 # * pow(10, 11)
@@ -48,7 +45,7 @@ def drawScene():
 
     for star in stars:
         glPushMatrix()
-        glTranslatef(star.x, star.y, star.z)
+        glTranslatef(star.pos[0], star.pos[1], star.pos[2])
         glutSolidSphere(0.1, 100, 100)
         glPopMatrix()
     
@@ -60,34 +57,19 @@ def update(value):
     for bstar in stars:
         for star in stars:
             if bstar != star:
-                if abs(star.x - bstar.x) < 0.5 and abs(star.y - bstar.y) < 0.5 and abs(star.z - bstar.z) < 0.5:
+                if abs(star.pos[0] - bstar.pos[0]) < 0.5 and abs(star.pos[1] - bstar.pos[1]) < 0.5 and abs(star.pos[2] - bstar.pos[2]) < 0.5:
                     stars.remove(star)
                     continue
-                d = math.sqrt(pow(star.x - bstar.x, 2) + pow(star.y - bstar.y, 2) + pow(star.z - bstar.z, 2))
-
-                ix = (star.x - bstar.x) / d
-                iy = (star.y - bstar.y) / d
-                iz = (star.z - bstar.z) / d
-
+                d = sqrt(pow(star.pos[0] - bstar.pos[0], 2) + pow(star.pos[1] - bstar.pos[1], 2) + pow(star.pos[2] - bstar.pos[2], 2))
+                i = ( (star.pos[0] - bstar.pos[0]) / d, (star.pos[1] - bstar.pos[1]) / d, (star.pos[2] - bstar.pos[2]) / d )
                 f = g * ((bstar.m * star.m) / (d*d))
-
-                fx = f * ix
-                fy = f * iy
-                fz = f * iz
-
-                ax = fx / bstar.m
-                ay = fy / bstar.m
-                az = fz / bstar.m
-
-                bstar.vx += ax
-                bstar.vy += ay
-                bstar.vz += az
+                f = (f * i[0], f * i[1], f * i[2])
+                a = (f[0] / bstar.m, f[1] / bstar.m, f[2] / bstar.m)
+                bstar.v = (bstar.v[0] + a[0], bstar.v[1] + a[1], bstar.v[2] + a[2]) 
 
     glutPostRedisplay();
     for star in stars:
-        star.x += star.vx
-        star.y += star.vy
-        star.z += star.vz
+        star.pos = (star.pos[0] + star.v[0], star.pos[1] + star.v[1], star.pos[2] + star.v[2]) 
     glutTimerFunc(25, update, 0)
 
 if __name__ == '__main__':
@@ -95,30 +77,18 @@ if __name__ == '__main__':
     stars = []
 
     star = Star()
-    star.x = 5.0
-    star.y = 0.0
-    star.z = 0.0
-    star.vx = 0.0
-    star.vy = 0.1
-    star.vz = 0.0
+    star.pos = (5.0, 0.0, 0.0)
+    star.v = (0.0, 0.1, 0.1)
     stars.append(star)
 
     star = Star()
-    star.x = -5.0
-    star.y = 0.0
-    star.z = 0.0
-    star.vx = 0.0
-    star.vy = -0.1
-    star.vz = 0.0
+    star.pos = (-5.0, 0.0, 0.0)
+    star.v = (0.0, -0.1, -0.1)
     stars.append(star)
     
     star = Star()
-    star.x = 0.0
-    star.y = 0.0
-    star.z = 0.0
-    star.vx = 0.0
-    star.vy = 0.0
-    star.vz = 0.0
+    star.pos = (0.0, 0.0, 0.0)
+    star.v = (0.0, 0.0, 0.0)
     stars.append(star)
 
     glutInit(sys.argv)
